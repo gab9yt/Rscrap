771d70ee712ca20058e4cd2f991dec834eb28ac5
gabin <104767385+gab9yt@users.noreply.github.com>
2026-06-14 13:36:20 +0200

    Delete core directory

--- core/__init__.py
+++
@@ -1,7 +0,0 @@
-from .database import Database
-from .cache import CacheManager
-from .audio_player import AudioPlayer
-from .music_api import MusicAPI
-from .virtual_list import VirtualTrackList
-
-__all__ = ["Database", "CacheManager", "AudioPlayer", "MusicAPI", "VirtualTrackList"]
